7bfe40cd929747596c5cb4d849aaec46546a5ef4
KostenkoNikita <neutralunit1187@gmail.com>
2017-02-26 03:06:33 +0200

    IO EnterText's
    and checking SelectionStart property in Input (it was impossible on Forms)

--- S-Calc/MainActivity.cs
+++ S-Calc/MainActivity.cs
@@ -1,18 +1,49 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views.InputMethods;
+using Android.Graphics;
+using Android.Content;
+using Android.Views;
+using RPNlib;
+using System;
 
 namespace S_Calc
 {
     [Activity(Label = "S_Calc", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        RPN_Real r;
+        EditText Input, Output;
+        string input => Input.Text;
+
+        private InputMethodManager imm;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            r = new RPN_Real();
+            Input = FindViewById<EditText>(Resource.Id.InputEditText);
+            Output = FindViewById<EditText>(Resource.Id.OutputEditText);
+            Output.Text = "Hello world";
+            imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+            Input.Click += EnterTextClick;
+            Output.Click += EnterTextClick;
+            Input.TextChanged += Input_TextChanged;
+        }
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+        private void Input_TextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            Output.Text = $"Input SelectionStart: {Input.SelectionStart}";
+        }
+
+        private void EnterTextClick(object sender, EventArgs e)
+        {
+            //Это прячет клавиатуру, работает через мат со второго нажатия и неизвестно как
+
+            //imm.HideSoftInputFromWindow(Input.WindowToken, 0);
+            //imm.HideSoftInputFromWindow(Output.WindowToken, 0);
         }
     }
 }
--- S-Calc/Resources/Resource.Designer.cs
+++ S-Calc/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace S_Calc
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int InputEditText = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int OutputEditText = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
